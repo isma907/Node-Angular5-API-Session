35975adadd5d8af812d9fd9b500d4957d8f9af4a
Ismael <isma907@gmail.com>
2018-05-20 22:01:15 -0300

    Hash Password on creating new user

--- models/User.js
+++ models/User.js
@@ -1,4 +1,5 @@
 var mongoose = require("mongoose");
+var bcrypt = require("bcrypt-nodejs")
 
 var userSchema = mongoose.Schema({
     _id: { type: mongoose.Schema.Types.ObjectId },
@@ -14,9 +15,22 @@ var userSchema = mongoose.Schema({
     Username: {
         type: String,
     },
-    password: {
+    Password: {
         type: String,
     },
 })
+
+
+//hashing a password before saving it to the database
+userSchema.pre('save', function (next) {
+    var user = this;
+    bcrypt.hash(user.Password, null, null, function (err, hash) {
+        if (err) {
+            return next(err);
+        }
+        user.Password = hash;
+        next();
+    })
+});
 
 var User = module.exports = mongoose.model('User', userSchema)
